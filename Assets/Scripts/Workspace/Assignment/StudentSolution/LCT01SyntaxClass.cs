f25991342a41d9b57cfaad90ce8a3dbd16efc90a
latina001 <nannaphat20160@gmail.com>
2026-09-01 12:34:22 +0700

    Start

--- Assets/Scripts/Workspace/Assignment/StudentSolution/LCT01SyntaxClass.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution/LCT01SyntaxClass.cs
@@ -5,15 +5,37 @@ namespace Assignment.StudentSolution.LCT01
 {
     public class Car
     {
+        public string Name;
+        public float Speed;
+        public string Color;
 
+        public void Move()
+        {
+            Debug.Log("Move");
+        }
+
+        public void Turn() {
+
+            Debug.Log("Turn");
+
+        }
     }
 
+    
     public class LCT01SyntaxClass
     {
         public void Start()
         {
             // Student code start HERE ...
+            Car car1 = new Car();
+            car1.Name = "F1";
+            car1.Speed = 100.0f; 
 
+            Car car2 = new Car();
+            car2.Name = "Toyota";
+
+            car1.Move();
+            car1.Turn();
             // Student code ends HERE 
         }
     }
